61d5f4f7c781c632af2f60636521161fe3516f41
atharav443 <atharavnaik3@gmail.com>
2022-09-08 01:59:32 +0530

    test case passed for negatives not allowed

--- string-calculator.js
+++ string-calculator.js
@@ -6,11 +6,11 @@ function StringCalculator() {
 
 
 StringCalculator.prototype.add = function(string_numbers) {
-    this.number=string_numbers;
-    if (this.number.includes('-')) {
-        throw new Error('negatives not allowed');
+  
+    if (string_numbers.includes('-')) {
+        return "Negatives not allowed";
         }
-    numbers = this.number.replace(/(\r\n]\n]\r)/gm,",");
+    numbers = string_numbers.replace(/(\r\n]\n]\r)/gm,",");
     if (numbers== '') {
         return 0;
     }
@@ -23,7 +23,8 @@ StringCalculator.prototype.add = function(string_numbers) {
         var arrayLength = res.length;
         
         for (var i = 0; i < res.length; i++) {
-          if(res[i]<1000){
+            
+            if(res[i]<1000){
             total = total + parseInt( res[i]);
           }
            
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -39,4 +39,7 @@ describe("String Calculator",function(){
    it("should ignore more than 1000 ",()=>{
     expect(calculator.add('10,1000')).toEqual(10);
    })
+   it("negative ",()=>{
+    expect(calculator.add('10,-1')).toEqual("Negatives not allowed")
+   })
 })
