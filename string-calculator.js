7bc757232235e50598fdb63b9a26a3c01695544e
atharav443 <atharavnaik3@gmail.com>
2022-09-07 19:00:06 +0530

    test case passed for string length 1

--- string-calculator.js
+++ string-calculator.js
@@ -10,6 +10,9 @@ StringCalculator.prototype.add = function(string_numbers) {
  if (string_numbers == '') {
         return 0;
     }
+    else if (string_numbers.length === 1){
+        return (parseInt(string_numbers));
+    } 
      
 };
 
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -9,9 +9,12 @@ describe("String Calculator",function(){
    })
    
    it("should return zero when '' is passed", function(){
-    expect(calculator.add('')).toEqual(0)
+    expect(calculator.add('')).toEqual(0);
    })
-
+   
+   it("should return integer when string length is 1",()=>{
+    expect(calculator.add('1')).toEqual(1);
+   })
 
 
 
